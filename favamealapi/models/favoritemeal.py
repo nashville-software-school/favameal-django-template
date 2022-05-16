e28bb2579015f1f3f7c6fa124fee321187ff5470
hannahhall <hannahkhall13@gmail.com>
2022-05-16 11:22:28 -0500

    updated models

--- favamealapi/models/favoritemeal.py
+++ favamealapi/models/favoritemeal.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class FavoriteMeal(models.Model):
+    """Favorite Meal model
+        Join model for the many to many relationship between user and meal
+    """
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="userfavoritemeals")
     meal = models.ForeignKey(
--- favamealapi/models/mealrating.py
+++ favamealapi/models/mealrating.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class MealRating(models.Model):
+    """Meal Rating model
+    Holds the rating a user has given a meal
+    """
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="mealrating")
     meal = models.ForeignKey(
